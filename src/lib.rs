5e30695ca783756f0ad1b50ee774bc7a31392a5a
Geordon Worley <vadixidav@gmail.com>
2020-05-29 18:01:55 -0400

    restart development of bitarray with const generics and byte length

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,57 @@
+#![no_std]
+#![allow(incomplete_features)]
 #![feature(const_generics)]
 
+use core::{
+    fmt,
+    hash::{Hash, Hasher},
+};
+
+/// A byte array with an alignment of 64 to maximize the efficiency of SIMD operations.
+#[repr(align(64))]
 pub struct ConstBytes<const N: usize>([u8; N]);
 
-/// A constant sized array of bits. `N` defines the number of bits in the array.
-pub struct BitArray<const N: usize> {
-    bytes: ConstBytes<{(N + 7) / 8}>,
+/// A constant sized array of bits. `B` defines the number of bytes.
+/// This has an alignment of 64 to maximize the efficiency of SIMD operations.
+/// It will automatically utilize SIMD at runtime where possible.
+#[repr(align(64))]
+#[derive(Clone)]
+pub struct BitArray<const B: usize> {
+    bytes: [u8; B],
 }
 
-impl<const N: usize> BitArray<{N}> {
-    fn new(bytes: ConstBytes<{(N + 7) / 8}>) -> Self {
-        Self {
-            bytes,
-        }
+impl<const B: usize> BitArray<B> {
+    pub fn new(bytes: [u8; B]) -> Self {
+        Self { bytes }
+    }
+}
+
+impl<const B: usize> PartialEq for BitArray<B> {
+    fn eq(&self, other: &Self) -> bool {
+        self.bytes
+            .iter()
+            .zip(other.bytes.iter())
+            .all(|(&a, &b)| a == b)
+    }
+}
+
+impl<const B: usize> Eq for BitArray<B> {}
+
+impl<const B: usize> fmt::Debug for BitArray<B> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.bytes[..].fmt(f)
+    }
+}
+
+impl<const B: usize> Hash for BitArray<B> {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.bytes[..].hash(state)
     }
 }
 
 #[cfg(test)]
 #[test]
 fn test_zeros() {
-    let array = BitArray::<5>::new(ConstBytes([0]));
-    assert!(&array.storage.0 == &[0u8]);
+    let array = BitArray::<1>::new([0]);
+    assert_eq!(array.bytes, [0]);
 }
