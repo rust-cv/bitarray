e649fcef0b8011da99eb08163165e66d49a32fec
Geordon Worley <vadixidav@gmail.com>
2020-05-30 00:26:03 -0400

    add distance method

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,12 @@
 #![no_std]
 #![allow(incomplete_features)]
-#![feature(const_generics)]
+#![feature(
+    const_generics,
+    link_llvm_intrinsics,
+    repr_simd,
+    simd_ffi,
+    platform_intrinsics
+)]
 
 use core::{
     fmt,
@@ -8,10 +14,26 @@ use core::{
     slice,
 };
 
+#[repr(simd)]
+#[derive(Copy, Clone)]
+struct Tup(u128, u128, u128, u128);
+
+#[allow(improper_ctypes, dead_code)]
+extern "C" {
+    #[link_name = "llvm.ctpop.v4i128"]
+    fn ctpop_512(x: Tup) -> Tup;
+    #[link_name = "llvm.experimental.vector.reduce.add.v4i128"]
+    fn reduce_add_512(x: Tup) -> u128;
+}
+
+extern "platform-intrinsic" {
+    fn simd_xor<T>(x: T, y: T) -> T;
+}
+
 /// A constant sized array of bits. `B` defines the number of bytes.
 /// This has an alignment of 64 to maximize the efficiency of SIMD operations.
 /// It will automatically utilize SIMD at runtime where possible.
-#[repr(align(8))]
+#[repr(align(64))]
 #[derive(Clone)]
 pub struct BitArray<const B: usize> {
     bytes: [u8; B],
@@ -58,21 +80,68 @@ impl<const B: usize> BitArray<B> {
     /// let array = BitArray::new([0xAA; 65]);
     /// assert_eq!(array.weight(), 4 * 65);
     /// ```
+    /// Compute the hamming weight of the `BitArray`.
+    ///
+    /// ```
+    /// use bitarray::BitArray;
+    /// let array = BitArray::new([0xAA; 65]);
+    /// assert_eq!(array.weight(), 4 * 65);
+    /// ```
     #[allow(clippy::cast_ptr_alignment)]
     pub fn weight(&self) -> usize {
-        let simd_len = B >> 3;
-        let simd_bytes = simd_len << 3;
+        let simd_len = B >> 6;
+        let simd_bytes = simd_len << 6;
         let simd_sum = unsafe {
-            slice::from_raw_parts(self.bytes.as_ptr() as *const u64, simd_len)
+            slice::from_raw_parts(self.bytes.as_ptr() as *const Tup, simd_len)
                 .iter()
                 .copied()
-                .map(|chunk| chunk.count_ones() as usize)
+                .map(|chunk| reduce_add_512(ctpop_512(chunk)) as usize)
                 .sum::<usize>()
         };
         let remaining_sum = self.bytes[simd_bytes..]
             .iter()
             .copied()
             .map(|b| b.count_ones() as usize)
+            .sum::<usize>();
+        simd_sum + remaining_sum
+    }
+
+    /// Compute the hamming distance to another `BitArray`.
+    ///
+    /// ```
+    /// use bitarray::BitArray;
+    ///
+    /// // All the bits are different.
+    /// let a = BitArray::new([0xAA; 65]);
+    /// let b = BitArray::new([0x55; 65]);
+    /// assert_eq!(a.distance(&b), 8 * 65);
+    ///
+    /// // None of the bits are different.
+    /// let a = BitArray::new([0xAA; 65]);
+    /// let b = BitArray::new([0xAA; 65]);
+    /// assert_eq!(a.distance(&b), 0);
+    /// ```
+    #[allow(clippy::cast_ptr_alignment)]
+    pub fn distance(&self, other: &Self) -> usize {
+        let simd_len = B >> 6;
+        let simd_bytes = simd_len << 6;
+        let simd_sum = unsafe {
+            slice::from_raw_parts(self.bytes.as_ptr() as *const Tup, simd_len)
+                .iter()
+                .copied()
+                .zip(
+                    slice::from_raw_parts(other.bytes.as_ptr() as *const Tup, simd_len)
+                        .iter()
+                        .copied(),
+                )
+                .map(|(a, b)| reduce_add_512(ctpop_512(simd_xor(a, b))) as usize)
+                .sum::<usize>()
+        };
+        let remaining_sum = self.bytes[simd_bytes..]
+            .iter()
+            .copied()
+            .zip(other.bytes[simd_bytes..].iter().copied())
+            .map(|(a, b)| (a ^ b).count_ones() as usize)
             .sum::<usize>();
         simd_sum + remaining_sum
     }
