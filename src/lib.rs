1a1808ceee7e43e342e293867a717e8b5e36bf09
Geordon Worley <vadixidav@gmail.com>
2020-09-11 18:53:54 -0400

    add unstable feature for faster matching and add to CI

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
 #![no_std]
 #![allow(incomplete_features)]
 #![feature(const_generics)]
+#![cfg_attr(
+    feature = "unstable-512-bit-simd",
+    feature(link_llvm_intrinsics, repr_simd, simd_ffi, platform_intrinsics)
+)]
 
 #[cfg(feature = "serde")]
 mod serde_impl;
@@ -15,12 +19,42 @@ use core::{
 #[cfg(feature = "space")]
 use space::MetricPoint;
 
-/// Split the bytes up into number of operations of size (512, 64, 8)
-const fn split_up_simd(n: usize) -> (usize, usize) {
-    let n_64 = n >> 3;
-    let bytes_64 = n_64 << 3;
-    let n_8 = n - bytes_64;
-    (n_64, n_8)
+cfg_if::cfg_if! {
+    if #[cfg(feature = "unstable-512-bit-simd")] {
+        #[repr(simd)]
+        #[derive(Copy, Clone)]
+        struct Tup(u128, u128, u128, u128);
+
+        #[allow(improper_ctypes, dead_code)]
+        extern "C" {
+            #[link_name = "llvm.ctpop.v4i128"]
+            fn ctpop_512(x: Tup) -> Tup;
+            #[link_name = "llvm.experimental.vector.reduce.add.v4i128"]
+            fn reduce_add_512(x: Tup) -> u128;
+        }
+
+        extern "platform-intrinsic" {
+            fn simd_xor<T>(x: T, y: T) -> T;
+        }
+
+        /// Split the bytes up into number of operations of size (512, 64, 8)
+        const fn split_up_simd(n: usize) -> (usize, usize, usize) {
+            let n_512 = n >> 6;
+            let bytes_512 = n_512 << 6;
+            let n_64 = (n - bytes_512) >> 3;
+            let bytes_64 = n_64 << 3;
+            let n_8 = n - bytes_512 - bytes_64;
+            (n_512, n_64, n_8)
+        }
+    } else {
+        /// Split the bytes up into number of operations of size (512, 64, 8)
+        const fn split_up_simd(n: usize) -> (usize, usize) {
+            let n_64 = n >> 3;
+            let bytes_64 = n_64 << 3;
+            let n_8 = n - bytes_64;
+            (n_64, n_8)
+        }
+    }
 }
 
 /// A constant sized array of bits. `B` defines the number of bytes.
@@ -88,22 +122,50 @@ impl<const B: usize> BitArray<B> {
     /// ```
     #[allow(clippy::cast_ptr_alignment)]
     pub fn weight(&self) -> usize {
-        let (n_64, n_8) = split_up_simd(self.bytes.len());
-        let sum_64 = unsafe {
-            slice::from_raw_parts(self.bytes.as_ptr() as *const u64, n_64)
-                .iter()
-                .copied()
-                .map(|chunk| chunk.count_ones() as usize)
-                .sum::<usize>()
-        };
+        cfg_if::cfg_if! {
+            if #[cfg(feature = "unstable-512-bit-simd")] {
+                let (n_512, n_64, n_8) = split_up_simd(self.bytes.len());
+                let sum_512 = unsafe {
+                    slice::from_raw_parts(self.bytes.as_ptr() as *const Tup, n_512)
+                        .iter()
+                        .copied()
+                        .map(|chunk| reduce_add_512(ctpop_512(chunk)) as usize)
+                        .sum::<usize>()
+                };
+                let sum_64 = unsafe {
+                    slice::from_raw_parts(self.bytes.as_ptr() as *const u64, n_64)
+                        .iter()
+                        .copied()
+                        .map(|chunk| chunk.count_ones() as usize)
+                        .sum::<usize>()
+                };
 
-        let sum_8 = self.bytes[self.bytes.len() - n_8..]
-            .iter()
-            .copied()
-            .map(|b| b.count_ones() as usize)
-            .sum::<usize>();
+                let sum_8 = self.bytes[self.bytes.len() - n_8..]
+                    .iter()
+                    .copied()
+                    .map(|b| b.count_ones() as usize)
+                    .sum::<usize>();
 
-        sum_64 + sum_8
+                sum_512 + sum_64 + sum_8
+            } else {
+                let (n_64, n_8) = split_up_simd(self.bytes.len());
+                let sum_64 = unsafe {
+                    slice::from_raw_parts(self.bytes.as_ptr() as *const u64, n_64)
+                        .iter()
+                        .copied()
+                        .map(|chunk| chunk.count_ones() as usize)
+                        .sum::<usize>()
+                };
+
+                let sum_8 = self.bytes[self.bytes.len() - n_8..]
+                    .iter()
+                    .copied()
+                    .map(|b| b.count_ones() as usize)
+                    .sum::<usize>();
+
+                sum_64 + sum_8
+            }
+        }
     }
 
     /// Compute the hamming distance to another `BitArray`.
@@ -123,12 +185,38 @@ impl<const B: usize> BitArray<B> {
     /// ```
     #[allow(clippy::cast_ptr_alignment)]
     pub fn distance(&self, other: &Self) -> usize {
-        self.bytes
-            .iter()
-            .copied()
-            .zip(other.bytes.iter().copied())
-            .map(|(a, b)| (a ^ b).count_ones() as usize)
-            .sum::<usize>()
+        cfg_if::cfg_if! {
+            if #[cfg(feature = "unstable-512-bit-simd")] {
+                let simd_len = B >> 6;
+                let simd_bytes = simd_len << 6;
+                let simd_sum = unsafe {
+                    slice::from_raw_parts(self.bytes.as_ptr() as *const Tup, simd_len)
+                        .iter()
+                        .copied()
+                        .zip(
+                            slice::from_raw_parts(other.bytes.as_ptr() as *const Tup, simd_len)
+                                .iter()
+                                .copied(),
+                        )
+                        .map(|(a, b)| reduce_add_512(ctpop_512(simd_xor(a, b))) as usize)
+                        .sum::<usize>()
+                };
+                let remaining_sum = self.bytes[simd_bytes..]
+                    .iter()
+                    .copied()
+                    .zip(other.bytes[simd_bytes..].iter().copied())
+                    .map(|(a, b)| (a ^ b).count_ones() as usize)
+                    .sum::<usize>();
+                simd_sum + remaining_sum
+            } else {
+                self.bytes
+                    .iter()
+                    .copied()
+                    .zip(other.bytes.iter().copied())
+                    .map(|(a, b)| (a ^ b).count_ones() as usize)
+                    .sum::<usize>()
+            }
+        }
     }
 }
 
